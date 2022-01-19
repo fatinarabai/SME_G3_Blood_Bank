4663b3b4c96bd005a5704771ae4d4b9c064c9522
Shuhani <shuhaniabdrani@gmail.com>
2022-01-19 21:33:52 +0800

    -task scheduling set to daily

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -26,7 +26,7 @@ class Kernel extends ConsoleKernel
     {
         // $schedule->command('inspire')
         //          ->hourly();
-        $schedule->call('App\Http\Controllers\AddressesDistrictController@updateDistrictCovidZone')->everyMinute();
+        $schedule->call('App\Http\Controllers\AddressesDistrictController@updateDistrictCovidZone')->daily();
     }
 
     /**
